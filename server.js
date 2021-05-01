67cbbe5085dc64a2c8af02893e487c84425582d1
dean <iamdekanos@gmail.com>
2021-05-01 17:35:45 +0300

    Added mongodb-atlas connection

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const dotenv = require("dotenv")
 const morgan = require('morgan')
+const connectDB = require('./config/db')
 
 // Load env files
 dotenv.config({ path: './config/config.env'})
+
+// Connect to database
+connectDB()
 
 // Route files
 const bootcamps = require('./routes/bootcamps')
@@ -20,4 +24,12 @@ if (process.env.NODE_ENV === 'development'){
 app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`)
+    // Close server & exit server
+    server.close(() => process.exit(1))
+})
