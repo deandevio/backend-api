f0dc93ff27cfffc52f63366f630ea495e870b026
dean <iamdekanos@gmail.com>
2021-05-01 18:10:15 +0300

    Added some data and models folder (inspied by MVC)

--- server.js
+++ server.js
@@ -1,35 +1,41 @@
-const express = require('express')
-const dotenv = require("dotenv")
-const morgan = require('morgan')
-const connectDB = require('./config/db')
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/db");
 
 // Load env files
-dotenv.config({ path: './config/config.env'})
+dotenv.config({ path: "./config/config.env" });
 
 // Connect to database
-connectDB()
+connectDB();
 
 // Route files
-const bootcamps = require('./routes/bootcamps')
+const bootcamps = require("./routes/bootcamps");
 
-const app = express()
+const app = express();
 
 // Dev logging middleware
-if (process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'))
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // Mount routers
 
-app.use('/api/v1/bootcamps', bootcamps)
+app.use("/api/v1/bootcamps", bootcamps);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.blue.bold
+  )
+);
 
 // Handle unhandled promise rejections
-process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error: ${err.message}`)
-    // Close server & exit server
-    server.close(() => process.exit(1))
-})
+process.on("unhandledRejection", (err, promise) => {
+console.log(`Error: ${err.message}`.red.bold);
+  // Close server & exit server
+  server.close(() => process.exit(1));
+});
