541a2b0b2bec2baa2e39afadd6f5d14e8f20c280
dean <iamdekanos@gmail.com>
2021-05-02 18:16:31 +0300

    updated CRUD functions to use the next() callback for error handling

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -8,7 +8,7 @@ exports.getBootcamps = async (req, res, next) => {
       .status(200)
       .send({ success: true, count: bootcamps.length, data: bootcamps });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(err);
   }
 };
 
@@ -20,7 +20,7 @@ exports.getBootcamp = async (req, res, next) => {
     }
     res.status(200).json({ success: true, data: bootcamp });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(err);
   }
 };
 
@@ -32,7 +32,7 @@ exports.createBootcamp = async (req, res, next) => {
       data: bootcamp,
     });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(err);
   }
 };
 
@@ -48,7 +48,7 @@ exports.updateBootcamp = async (req, res, next) => {
     }
     res.status(200).json({ success: true, data: bootcamp });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(err);
   }
 };
 
@@ -61,6 +61,6 @@ exports.deleteBootcamp = async (req, res, next) => {
     }
     res.status(200).json({ success: true, data: {} });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(err);
   }
 };
