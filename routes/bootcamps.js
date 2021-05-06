2180bacc13986bb15089263dba311566f67748a6
dean <iamdekanos@gmail.com>
2021-05-06 18:54:06 +0300

    Added import data funcionality and a new route for getting bootcamps by distance

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -6,8 +6,10 @@ const {
   updateBootcamp,
   deleteBootcamp,
   createBootcamp,
+  getBootcampsInRadius,
 } = require("../controllers/bootcamp");
 
+router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
 router.route("/").get(getBootcamps).post(createBootcamp);
 router.route("/:id").get(getBootcamp).delete(deleteBootcamp).put(updateBootcamp);
 
