940da20fd996cabc873c0751eb440dc43009ba6d
dean <iamdekanos@gmail.com>
2021-05-08 20:48:08 +0300

    Basic edits

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# This is the readme file
+# Backend API for a bootcamp website selling courses.
--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
+// Initializing the server
 const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.blue.bold)
